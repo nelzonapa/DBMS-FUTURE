e76dcfe97ffcfc84d0d3a4917f17e9d01289a1a0
Nelzon Apaza <84747728+nelzonapa@users.noreply.github.com>
2023-06-30 15:25:53 -0500

    Archivos para cada parte del Disco puestas

---
+++ Future_DBMS_V.2.0/Magnetic_Disk/Sistema_Operativo.h
@@ -0,0 +1,16 @@
+#ifndef SISTEMA_OPERATIVO_H
+#define SISTEMA_OPERATIVO_H
+
+class Sistema_Operativo
+{
+private:
+    /* data */
+public:
+    Sistema_Operativo();
+    ~Sistema_Operativo();
+
+    /*Funciones*/
+
+};
+
+#endif 
